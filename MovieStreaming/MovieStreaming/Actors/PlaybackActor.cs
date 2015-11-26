d58c5f45142699db290364d3a32ce71b0dca3dc2
Anton Szórád <anton.szorad@gmail.com>
2015-11-26 15:33:10 +0100

    MovieStreaming example - Basic structure of Actor system

---
+++ MovieStreaming/MovieStreaming/Actors/PlaybackActor.cs
@@ -0,0 +1,30 @@
+﻿using Akka.Actor;
+
+namespace MovieStreaming.Actors
+{
+    using System;
+
+    using MovieStreaming.Messages;
+
+    public class PlaybackActor : UntypedActor
+    {
+        public PlaybackActor()
+        {
+            Console.WriteLine("Creating a PlaybackActor");
+        }
+        protected override void OnReceive(object message)
+        {
+            var movieMessage = message as PlayMovieMessage;
+            if (movieMessage != null)
+            {
+                var mes = movieMessage;
+                Console.WriteLine("Received movie title: " + mes.MovieTitle);
+                Console.WriteLine("Received user Id:  " + mes.UserId);
+            }
+            else
+            {
+                Unhandled(message);
+            }
+        }
+    }
+}
---
+++ MovieStreaming/MovieStreaming/Program.cs
@@ -0,0 +1,29 @@
+﻿using Akka.Actor;
+using System;
+
+namespace MovieStreaming
+{
+    using MovieStreaming.Actors;
+    using MovieStreaming.Messages;
+
+    class Program
+    {
+        private static ActorSystem movieStreamingActorSystem;
+
+        static void Main(string[] args)
+        {
+            movieStreamingActorSystem = ActorSystem.Create("movieStreamingActorSystem");
+            Console.WriteLine("Actor system created");
+
+            Props playabackActorProps = Props.Create<PlaybackActor>();
+
+            IActorRef playbackActorRef = movieStreamingActorSystem.ActorOf(playabackActorProps, "PlaybackActor");
+
+            playbackActorRef.Tell(new PlayMovieMessage("Akka.NET: The Movie", 42));
+
+            Console.ReadLine();
+
+            movieStreamingActorSystem.Shutdown();
+        }
+    }
+}
