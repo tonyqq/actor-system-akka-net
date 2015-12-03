2c6d4b8dde6d724aef61fb9e99e27d38a9413769
Anton Szórád <anton.szorad@gmail.com>
2015-12-03 17:36:13 +0100

    Added custom supervisor OneForOneStrategy to PlaybackStatisticsActor

--- MovieStreaming/MovieStreaming/Actors/PlaybackStatisticsActor.cs
+++ MovieStreaming/MovieStreaming/Actors/PlaybackStatisticsActor.cs
@@ -4,11 +4,33 @@ using Akka.Actor;
 
 namespace MovieStreaming.Actors
 {
+    using MovieStreaming.Exceptions;
+
     public class PlaybackStatisticsActor : ReceiveActor
     {
         public PlaybackStatisticsActor()
         {
             Context.ActorOf(Props.Create<MoviePlayCounterActor>(), "MoviePlayCounter");
+        }
+
+        protected override SupervisorStrategy SupervisorStrategy()
+        {
+            return new OneForOneStrategy(
+                exception =>
+                    {
+                        if (exception is SimulatedCorruptStateException)
+                        {
+                            return Directive.Restart;
+                        }
+
+                        if (exception is SimulatedTerribleMovieException)
+                        {
+                            return Directive.Resume;
+                        }
+
+                        return Directive.Restart;
+                    }
+                );
         }
 
         protected override void PreStart()
